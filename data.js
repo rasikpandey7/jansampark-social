171612f4c0d2872001432dd0d782a37c0b5d19f3
Rasik Pandey <55084127+rasikpandey7@users.noreply.github.com>
2026-01-19 18:57:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+/* ==============================
+   Jansampark MP – Account Data
+   ============================== */
+
+/* Government Departments */
+const departments = [
+  {
+    name: "Home Department, Madhya Pradesh",
+    twitter: "https://twitter.com/",
+    facebook: "https://facebook.com/"
+  },
+  {
+    name: "Health Department, Madhya Pradesh",
+    twitter: "https://twitter.com/",
+    facebook: "https://facebook.com/"
+  }
+];
+
+/* District Administration */
+const districts = [
+  {
+    name: "Bhopal Collector",
+    twitter: "https://twitter.com/"
+  },
+  {
+    name: "Indore Collector",
+    twitter: "https://twitter.com/"
+  }
+];
+
+/* Commissioners */
+const commissioners = [
+  {
+    name: "Bhopal Division Commissioner",
+    twitter: "https://twitter.com/"
+  }
+];
+
+/* Joint Directors */
+const jointDirectors = [
+  {
+    name: "Joint Director, Jansampark",
+    twitter: "https://twitter.com/"
+  }
+];
